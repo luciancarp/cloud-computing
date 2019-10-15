6bd780cb790a35dbefc03bf363e1926b5ee25d35
Lucian Carp <lc16929@my.bristol.ac.uk>
2019-10-15 17:02:50 +0100

    Improve

--- nonce.py
+++ nonce.py
@@ -1,6 +1,6 @@
 import hashlib
 import logging
-import multiprocessing
+from multiprocessing import Process, Pipe, Value, cpu_count
 import datetime
 
 
@@ -18,12 +18,12 @@ def find_nonce(block, start_nonce, d):
         string = str(block)+str(nonce)
         hash = sha256_squared(string)
 
-        leading_zeros = 1
+        has_leading_zeros = 1
         for i in range(d):
             if (hash[i // 8] >> (i % 8)) & 1 == 1:
-                leading_zeros = 0
+                has_leading_zeros = 0
 
-        if leading_zeros == 1:
+        if has_leading_zeros == 1:
             found = 1
         else:
             nonce += 1
@@ -31,63 +31,51 @@ def find_nonce(block, start_nonce, d):
     return nonce
 
 
-def thread(name, start_nonce, max_nonce, block, d):
-    # logging.info("Thread %s: starting", name)
+def thread(name, conn, start_nonce, max_nonce, block, d):
     time_start = datetime.datetime.now()
     print("%s: Thread %d: starting" % (time_start, name))
     nonce = find_nonce(block, start_nonce, d)
-    # logging.info("Thread %s: found nonce: %d", name, nonce)
-    # logging.info("Thread %s: finishing", name)
-    time_end = datetime.datetime.now()
-    print("%s: Thread %d: found nonce: %d" % (time_end, name, nonce))
-    print("%s: Thread %d: finishing" % (time_end, name))
-    print("%s: Thread %d: Time Elapsed: %s" %
-          (time_end, name, time_end - time_start))
+    if nonce != start_nonce:
+        time_end = datetime.datetime.now()
+        print("%s: Thread %d: found nonce: %d" % (time_end, name, nonce))
+        print("%s: Thread %d: finishing" % (time_end, name))
+        print("%s: Thread %d: Time Elapsed: %s" %
+              (time_end, name, time_end - time_start))
+        conn.send(nonce)
 
 
 if __name__ == "__main__":
+
     block = 123
     nonce = 0
     max_nonce = 4294967296
-    threads_num = 8
+    threads_num = cpu_count()
     d = 32
 
-    # format = "%(asctime)s: %(message)s"
-    # logging.basicConfig(format=format, level=logging.INFO,
-    #                     datefmt="%H:%M:%S")
+    final_nonce = 0
 
-    # time_start = datetime.datetime.now()
-
-    nonce_thread = int(max_nonce / threads_num)
+    nonce_thread = max_nonce // threads_num
 
     threads = list()
+    parent_conn, child_conn = Pipe()
+
     for index in range(threads_num):
-        # logging.info("Main    : create and start thread %d.", index)
+
+        max_nonce_thread = max_nonce
+        if index != (threads_num - 1):
+            max_nonce_thread = nonce_thread * (index + 1) - 1
+
         time = datetime.datetime.now()
         print("%s: Main: Create and start Thread %d" % (time, index))
-        x = multiprocessing.Process(target=thread, args=(
-            index, nonce_thread * index, nonce_thread * (index + 1) - 1, block, d))
+
+        x = Process(target=thread, args=(
+            index, child_conn, nonce_thread * index, max_nonce_thread, block, d))
+
         threads.append(x)
         threads[index].start()
 
-    # p1 = multiprocessing.Process(target=thread, args=(
-    #     0, nonce_thread * 0, nonce_thread * (0 + 1) - 1, block, d))
+    final_nonce = parent_conn.recv()
+    for index in range(threads_num):
+        threads[index].terminate()
 
-    # p2 = multiprocessing.Process(target=thread, args=(
-    #     1, nonce_thread * 1, nonce_thread * (1 + 1) - 1, block, d))
-
-    # p3 = multiprocessing.Process(target=thread, args=(
-    #     2, nonce_thread * 2, nonce_thread * (2 + 1) - 1, block, d))
-
-    # p4 = multiprocessing.Process(target=thread, args=(
-    #     3, nonce_thread * 3, nonce_thread * (3 + 1) - 1, block, d))
-
-    # p1.start()
-    # p2.start()
-    # p3.start()
-    # p4.start()
-    # thread('hello', 0, 150, block, d)
-
-    # time_end = datetime.datetime.now()
-
-    # print("Elapsed time: %s\n" % (time_end - time_start))
+    print('Final nonce: ', final_nonce)
